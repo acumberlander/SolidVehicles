604136e1e10a0014978fe34377005585f0aa9824
Austin <austincumberlander@gmail.com>
2019-03-30 14:37:49 -0500

    successfully created list. wip

--- SolidVehicles/Program.cs
+++ SolidVehicles/Program.cs
@@ -1,12 +1,245 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace SolidVehicles
+public interface IVehicle
 {
-    class Program
+    //Properties
+
+    int Wheels { get; set; }
+    int Doors { get; set; }
+    int PassengerCapacity { get; set; }
+    bool Winged { get; set; }
+    string TransmissionType { get; set; }
+    double EngineVolume { get; set; }
+    double MaxWaterSpeed { get; set; }
+    double MaxLandSpeed { get; set; }
+    double MaxAirSpeed { get; set; }
+
+    void Fly();
+    void Start();
+    void Stop();
+}
+public class JetSki : IVehicle
+{
+    public int Wheels { get; set; }
+    public int Doors { get; set; }
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; }
+    public string TransmissionType { get; set; }
+    public double EngineVolume { get; set; }
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
     {
-        static void Main(string[] args)
+        Console.WriteLine("The jetski zips through the waves with the greatest of ease");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class SpeedBoat : IVehicle
+{
+    public int Wheels { get; set; }
+    public int Doors { get; set; }
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; }
+    public string TransmissionType { get; set; }
+    public double EngineVolume { get; set; }
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
+    {
+        Console.WriteLine("The jetski zips through the waves with the greatest of ease");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Motorcycle : IVehicle
+{
+    public int Wheels { get; set; } = 2;
+    public int Doors { get; set; } = 0;
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; } = false;
+    public string TransmissionType { get; set; } = "Manual";
+    public double EngineVolume { get; set; } = 1.3;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; } = 160.4;
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
+    {
+        Console.WriteLine("The motorcycle screams down the highway");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}public class Car : IVehicle
+{
+    public int Wheels { get; set; } = 2;
+    public int Doors { get; set; } = 0;
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; } = false;
+    public string TransmissionType { get; set; } = "Manual";
+    public double EngineVolume { get; set; } = 1.3;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; } = 160.4;
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
+    {
+        Console.WriteLine("The motorcycle screams down the highway");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Cessna : IVehicle
+{
+    public int Wheels { get; set; } = 3;
+    public int Doors { get; set; } = 3;
+    public int PassengerCapacity { get; set; } = 113;
+    public bool Winged { get; set; } = true;
+    public string TransmissionType { get; set; } = "None";
+    public double EngineVolume { get; set; } = 31.1;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; } = 309.0;
+
+    public void Drive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("The Cessna effortlessly glides through the clouds like a gleaming god of the Sun");
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Boeing : IVehicle
+{
+    public int Wheels { get; set; } = 3;
+    public int Doors { get; set; } = 3;
+    public int PassengerCapacity { get; set; } = 113;
+    public bool Winged { get; set; } = true;
+    public string TransmissionType { get; set; } = "None";
+    public double EngineVolume { get; set; } = 31.1;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; } = 309.0;
+
+    public void Drive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("The Boeing effortlessly glides through the clouds like a gleaming god of the Sun");
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+        // Build a collection of all vehicles that fly
+        List<object> Flyers = new List<object>();
+
+        Boeing boeing = new Boeing();
+        Cessna cessna = new Cessna();
+
+        Flyers.Add(boeing);
+        Flyers.Add(cessna);
+
+        // With a single `foreach`, have each vehicle Fly()
+        foreach (object aircraft in Flyers)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(aircraft);
         }
+
+        // Build a collection of all vehicles that operate on roads
+
+        // With a single `foreach`, have each road vehicle Drive()
+
+
+
+        // Build a collection of all vehicles that operate on water
+
+        // With a single `foreach`, have each water vehicle Drive()
     }
 }
